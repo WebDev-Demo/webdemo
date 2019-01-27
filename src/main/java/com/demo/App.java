f4b47c96d050030ed5fe765753706d8f17cabbb9
Emy Tang <emytang@Mans-MacBook-Air.local>
2019-01-27 10:41:02 -0800

    added html and js

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,21 +1,18 @@
 package com.demo;
 
-import org.springframework.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
 /**
  * Hello world!
- *
  */
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class App 
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
